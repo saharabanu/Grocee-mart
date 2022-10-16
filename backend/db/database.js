3efd72c182eb7a9552c5f0c59f948580e7ce3f1f
saharabanu <saharabanungn@gmail.com>
2022-10-16 12:32:25 +0600

    env user pass adedd

--- backend/db/database.js
+++ backend/db/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
 
     try {
-        mongoose.connect("mongodb+srv://DevZeroOne:DevZeroOne@cluster0.2kdey3p.mongodb.net/Dev_Commerce?retryWrites=true&w=majority")
+        mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2kdey3p.mongodb.net/Dev_Commerce?retryWrites=true&w=majority`)
             .then(data => {
                 console.log(`MongoDB connected with server: ${data.connection.host}`)
             })
